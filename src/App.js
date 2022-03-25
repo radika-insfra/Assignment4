d1524a9461185cb6b6e891f6de9ab97859edb02f
Cal-Ell <calvin.malagon@ultimagen.com>
2022-03-24 21:52:10 -0700

    card

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Card from './Components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
